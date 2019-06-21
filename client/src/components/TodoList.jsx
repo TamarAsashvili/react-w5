cfc0d67791360100e0c1b949c135738d4926c331
TamarAsashvili <asashvili@gmail.com>
2019-06-21 22:00:10 +0200

    5

---
+++ client/src/components/TodoList.jsx
@@ -0,0 +1,116 @@
+import React from "react";
+import { AddItem } from "./AddItem";
+import { TodoItem } from "./TodoItem";
+
+export class TodoList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      items: []
+    };
+  }
+
+  componentDidMount = async () => {
+    const data = await fetch("/api/todos");
+    const jsonData = await data.json();
+    this.setState({
+      items: jsonData
+    });
+  };
+
+  // Step 1: change the onAddItem function to update the items on the server with a request
+
+  onAddItem = async newToDo => {
+    let items = this.state.items;
+    const response = await fetch("/api/todos", {
+      method: "POST",
+      body: JSON.stringify({
+        id: items.length + 1,
+        title: newToDo,
+        completed: false
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    const newItems = await response.json();
+    this.setState({ items: newItems });
+    console.log(items);
+  };
+
+  // FYI: here is a small example about how to handle the update of an item. Take inspiration from it to build the other requests!
+
+  handleChecked = async index => {
+    let item = this.state.items[index];
+    item.completed = !item.completed;
+    const response = await fetch(`/api/todos/${item.id}`, {
+      method: "PUT",
+      body: JSON.stringify(item), // Coordinate the body type with 'Content-Type'
+      headers: new Headers({
+        "Content-Type": "application/json"
+      })
+    });
+    const items = await response.json();
+    this.setState({
+      items
+    });
+  };
+
+  // Step 3: Do the same as you did on the client and on the server for handling the delete feature
+
+  handleDelete = async index => {
+    let item = this.state.items[index];
+    const response = await fetch(`/api/todos/${item.id}`, {
+      method: "DELETE",
+      body: JSON.stringify(item),
+      headers: new Headers({
+        "Content-Type": "application/json"
+      })
+    });
+    const items = await response.json();
+    this.setState({
+      items
+    });
+  };
+  // Step 5: update an item by doing the same call as we did for handling the check/uncheck feature. Here you only need to change the client side since the server side is already done ;)
+
+  handleEdit = async (index, title) => {
+    let items = this.state.items;
+    let item = items[index];
+    item.title = title;
+    const response = await fetch(`/api/todos/${item.id}`, {
+      method: "PUT",
+      body: JSON.stringify(item),
+      headers: new Headers({
+        "Content-Type": "application/json"
+      })
+    });
+    const newItems = await response.json();
+    this.setState({
+      items: newItems
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <AddItem onAddItem={this.onAddItem} />
+        <ul>
+          {this.state.items.map((value, index) => {
+            return (
+              <TodoItem
+                handleChecked={this.handleChecked}
+                handleDelete={this.handleDelete}
+                handleEdit={this.handleEdit}
+                title={value.title}
+                completed={value.completed}
+                id={index}
+                key={index} // you can't use it. Just used by map to optimize the rendering
+              />
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+}
---
+++ server/controllers/todos.js
@@ -0,0 +1,45 @@
+let todos = [
+  {
+    id: 1,
+    title: "Learn React",
+    completed: true
+  },
+  {
+    id: 2,
+    title: "Learn to connect React and Node",
+    completed: false
+  }
+];
+
+// Step 2: update the create request to be able to add an item to the list
+const create = (req, res) => {
+  todos.push(req.body);
+  res.status(200).send(todos);
+};
+const update = (req, res) => {
+  for (let i = 0; i < todos.length; i++) {
+    if (todos[i].id === Number(req.params.todoId)) todos[i] = req.body;
+  }
+  res.status(200).send(todos);
+};
+
+const list = (req, res) => {
+  res.status(200).send(todos);
+};
+
+// Step 4: implement the delete feature here.
+
+const destroy = (req, res) => {
+  const index = todos.findIndex(
+    todo => todo.id === parseInt(req.params.todoId)
+  );
+  todos.splice(index, 1);
+  res.status(200).send(todos);
+};
+
+module.exports = {
+  list,
+  create,
+  update,
+  destroy
+};
